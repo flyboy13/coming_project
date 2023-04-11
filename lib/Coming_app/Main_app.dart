48a03ec45b036efc504db6357c502184e244ee6f
Nguyen Duc Tai <myanimalclass@gmail.com>
2023-04-11 21:22:30 +0700

    bottom bar

---
+++ lib/Coming_app/Main_app.dart
@@ -0,0 +1,112 @@
+// ignore_for_file: file_names
+
+import 'package:coming_project/Coming_app/models/tabIcon_data.dart';
+// import 'package:coming_project/Coming_app/NotiScreen/Noti_Screen.dart';
+import 'package:flutter/material.dart';
+import 'bottom_navigation_view/bottom_bar_view.dart';
+import 'Main_app_theme.dart';
+import 'Home_screen/Home_Screen.dart';
+
+class ComingAppHomeScreen extends StatefulWidget {
+  const ComingAppHomeScreen({super.key});
+
+  @override
+  ComingAppHomeScreenState createState() => ComingAppHomeScreenState();
+}
+
+class ComingAppHomeScreenState extends State<ComingAppHomeScreen>
+    with TickerProviderStateMixin {
+  AnimationController? animationController;
+
+  List<TabIconData> tabIconsList = TabIconData.tabIconsList;
+
+  Widget tabBody = Container(
+    color: ComingAppTheme.background,
+  );
+
+  @override
+  void initState() {
+    for (var tab in tabIconsList) {
+      tab.isSelected = false;
+    }
+    tabIconsList[0].isSelected = true;
+
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 600), vsync: this);
+    tabBody = Home_Screen(animationController: animationController);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: ComingAppTheme.background,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: FutureBuilder<bool>(
+          future: getData(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (!snapshot.hasData) {
+              return const SizedBox();
+            } else {
+              return Stack(
+                children: <Widget>[
+                  tabBody,
+                  bottomBar(),
+                ],
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 200));
+    return true;
+  }
+
+  Widget bottomBar() {
+    return Column(
+      children: <Widget>[
+        const Expanded(
+          child: SizedBox(),
+        ),
+        BottomBarView(
+          tabIconsList: tabIconsList,
+          addClick: () {},
+          changeIndex: (int index) {
+            if (index == 0 || index == 2) {
+              animationController?.reverse().then<dynamic>((data) {
+                if (!mounted) {
+                  return;
+                }
+                setState(() {
+                  tabBody =
+                      Home_Screen(animationController: animationController);
+                });
+              });
+            } else if (index == 1 || index == 3) {
+              animationController?.reverse().then<dynamic>((data) {
+                if (!mounted) {
+                  return;
+                }
+                setState(() {
+                  tabBody =
+                      Home_Screen(animationController: animationController);
+                });
+              });
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
